d57296836ade3d528093083dae5d20847c57a927
Ramon Marques <ramon.junior7@enova.educacao.ba.gov.br>
2020-11-07 06:09:23 -0300

    adicionado cors

--- .history/src/app_20201031190401.ts
+++ .history/src/app_20201031190401.ts
@@ -14,7 +14,6 @@ createConnection();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
